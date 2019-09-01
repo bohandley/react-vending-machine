1754a488e2dc86dc6d9761695197a77d297ba5dc
Brendan O'Handley <brendan.ohandley@gmail.com>
2019-09-01 00:09:27 -0400

    add multiple images when stock is greater than 1 in window

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,20 +1,38 @@
 import React from 'react';
 
 class Product extends React.Component {
+    backgroundImgs(num, img){
+        let amt = num == "Sold Out" ? 0 : 
+            num > 3 ? 4 : num;
 
+        let imgs = [];
+
+        let count = 1;
+
+        while (count < amt) {
+            let cls = "prod-bkgrd-img-" + count;
+            imgs.push(<img class={cls} src={img} />)
+            count += 1;
+        }
+
+        return imgs;
+    }
 
     render() {
+        let imgs = this.backgroundImgs(this.props.num, this.props.img);
+
         return(
             <div class={this.props.class}>
                 <div class="prod-wrapper">
                     <div>
-                        {this.props.text}<br/>{this.props.price}
+                        {this.props.price}
                     </div>
                     <div>
                         <img class="prod-img" src={this.props.img}/>
+                        {imgs}
                     </div>
                     <div>
-                        Qty. {this.props.num}
+                        {this.props.num != "Sold Out" ? "Qty. " + this.props.num : this.props.num}
                     </div>
                     <div>
                         {this.props.code}
